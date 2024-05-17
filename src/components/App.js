ab63cb9ad40598e62b90e7e957de00ce19ba2d31
Timothy Barasa <timbaros87@gmail.com>
2024-05-17 23:40:31 +0300

    Create and update the ArticleList

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import blogData from "../data/blog";
 import Header from "./Header";
 import About from "./About";
 import logo from "../assets/logo";
+import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
@@ -10,7 +11,8 @@ function App() {
   return (
     <div className="App">
       <Header name={blogData.name} />
-      <About image= {blogData.image || logo} about={blogData.about} />
+      <About image={blogData.image || logo} about={blogData.about} />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
---
+++ src/components/ArticleList.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function ArticleList() {
+    return (
+        <main>
+            <h2>Articles</h2>
+            <ul>
+                <li>Article 1</li>
+                <li>Article 2</li>
+                <li>Article 3</li>
+                <li>Article 4</li>
+                <li>Article 5</li>
+                <li>Article 6</li>
+            </ul>
+        </main>
+    )
+}
+
+export default ArticleList;
