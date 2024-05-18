ec2ca27d6001bacb339f6a12d2a4b08550b1c349
Timothy Barasa <timbaros87@gmail.com>
2024-05-18 17:40:03 +0300

    Update the Article component to calcualte emojis

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
 import React from "react";
-import blogData from "../data/blog";
 
 function About({ image = "https://via.placeholder.com/215", About }) {
     return (
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,28 @@
 import React from "react";
 
-function Article({ title, date = "January 1, 1970", preview }) {
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+    let emoji = "";
+    let count = 0;
+    if (minutes < 30) {
+        emoji = " ☕️ ";
+        count = Math.ceil(minutes / 5);
+    } else {
+        emoji = "🍱";
+        count = Math.ceil(minutes / 10);
+    }
+
+    const emojiString = emoji.repeat(count);
+
     return (
-        <article>
-            <h3>{title}</h3>
-            <small> {date} </small>
-            <p> {preview} </p>
-        </article>
-    )
+      <article>
+        <h3>{title}</h3>
+        <small>
+          {" "}
+          {date} • {emojiString} {minutes} min read
+        </small>
+        <p> {preview} </p>
+      </article>
+    );
 }
 
 export default Article;
